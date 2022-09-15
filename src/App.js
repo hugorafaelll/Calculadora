f14c6ec0632ef97696a716b450e1c081f52cbdb8
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-09-15 15:35:02 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Calculadora from './components/Calculator';
+
+function App() {
+  return (
+    
+    <Calculadora/>
+  )
+}
+
+export default App;
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Calculator.css'
+
+const Calculadora  = ()=> {
+  return (
+    <div className='wrapper'>
+      <button >1</button>
+      <button >2</button>
+      <button >3</button><br></br>
+      <button >4</button>
+      <button >5</button>
+      <button >6</button><br></br>
+      <button >7</button>
+      <button >8</button>
+      <button >9</button>
+      <button >0</button>
+      <button >AC</button>
+      <button >+/-</button>
+      <button >%</button>
+      <button >,</button>
+      <button >/</button>
+      <button >x</button>
+      <button >-</button>
+      <button >+</button>
+      <button >=</button>
+    </div>
+  )
+}
+
+export default Calculadora  
